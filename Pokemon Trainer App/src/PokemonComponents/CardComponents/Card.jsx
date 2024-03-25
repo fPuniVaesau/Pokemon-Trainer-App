cbaa23b2bb90b6ad8e2b817448b5dfc7e7f21de5
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-25 16:25:25 -0700

    Working on the card component for the application.

--- Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
+++ Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 export default function Card(){
     let [pokemon, setPokemon] = useState('');
+    useEffect(()=>{
+
+    }, []);
+
 
     return(
         <div>
             <h2>Blastoise</h2>
             <img src="https://i.pinimg.com/originals/d3/6e/6f/d36e6f74d9ade37082eb7234cb6dcc22.png" alt="pokemon" />
+            <button>Search Pokedex</button>
         </div>
     )
 }
