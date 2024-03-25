c40b7449a59c17966ae47aab8520b13bbccc5882
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-25 16:22:15 -0700

    Adding the files to the repo on github.

--- Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
+++ Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
@@ -3,8 +3,8 @@ export default function Card(){
 
     return(
         <div>
-            <h2>Pokemon Name</h2>
-            <img src="https://i.pinimg.com/originals/d3/6e/6f/d36e6f74d9ade37082eb7234cb6dcc22.png" alt="" />
+            <h2>Blastoise</h2>
+            <img src="https://i.pinimg.com/originals/d3/6e/6f/d36e6f74d9ade37082eb7234cb6dcc22.png" alt="pokemon" />
         </div>
     )
 }
