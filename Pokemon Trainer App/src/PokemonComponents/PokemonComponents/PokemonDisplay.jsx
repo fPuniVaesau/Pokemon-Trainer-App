b86b4e0e5eab705b6e08f669f6aadf4386c90458
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-26 16:53:34 -0700

    Working on the card component for the application.

--- Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonDisplay.jsx
+++ Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonDisplay.jsx
@@ -1,10 +1,12 @@
+import styles from "./PokemonDisplay.module.css"
+
 export default function PokemonDisplay({ pokemonData, setPokemonData }) {
   return (
     <div>
       {pokemonData.map((pokemonChoice) => (
         <div>
           <p>{pokemonChoice.name}</p>
-          <img src={pokemonChoice.sprites.front_default} alt='' />
+          <img src={pokemonChoice.sprites.front_default} alt='pokemon image' />
         </div>
       ))}
     </div>
