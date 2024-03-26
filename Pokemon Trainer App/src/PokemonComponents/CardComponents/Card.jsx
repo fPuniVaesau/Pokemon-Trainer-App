d5b9618890f8183134f7beddfc635c01008bcc3d
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-25 21:00:51 -0700

    Working on the card component for the application.

--- Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
+++ Pokemon Trainer App/src/PokemonComponents/CardComponents/Card.jsx
@@ -1,34 +1,34 @@
 import { useState, useEffect } from 'react';
-export default function Card({pokemon, setPokemon}) {
-   let baseURL = "https://pokeapi.co/api/v2/pokemon/"
-  
+export default function Card({ pokemon, setPokemon }) {
+  let baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+
   useEffect(() => {
     fetchPokemonData();
-    
-    async function fetchPokemonData(){
-        try {
-            let response = await fetch(`${baseURL}${pokemon.name}`)
-            if (!response.ok){
-                throw new Error("Pokemon Data Not Found!");
-            }
-            let data = await response.json();
-            console.log(data)
-        } catch (error) {
-            console.log(error)
+
+    async function fetchPokemonData() {
+      try {
+        let response = await fetch(`${baseURL}${pokemon.name}`);
+        if (!response.ok) {
+          throw new Error('Pokemon Data Not Found!');
         }
+        let data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
     }
   }, [pokemon]);
 
   //Functions for handling events
   let handleChange = (e) => {
-    setPokemon({...pokemon, name:e.target.value})
-    console.log(pokemon.name)
-  }
+    setPokemon({ ...pokemon, name: e.target.value });
+    console.log(pokemon.name);
+  };
 
   let handleSearch = (e) => {
     e.preventDefault();
-    setPokemon('')
-  }
+    setPokemon('');
+  };
 
   return (
     <div>
@@ -37,7 +37,7 @@ export default function Card({pokemon, setPokemon}) {
         <input type='text' value={pokemon.name} onChange={handleChange} />
         <button>Search Pokedex</button>
       </form>
-        {pokemon.name}
+      {pokemon.name}
     </div>
   );
 }
