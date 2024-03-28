910b637db880e713885db78aec2f8b480178ba07
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-27 17:03:04 -0700

    Working on the card component for the application.

--- Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonDisplay.jsx
+++ Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonDisplay.jsx
@@ -1,11 +1,16 @@
-import PokemonInfo from "./PokemonInfoComponents/PokemonInfo";
-import styles from "./PokemonDisplay.module.css"
+import PokemonInfo from './PokemonInfoComponents/PokemonInfo';
+import styles from './PokemonDisplay.module.css';
 
 export default function PokemonDisplay({ pokemonData, setPokemonData }) {
   return (
     <div className={styles.container}>
       {pokemonData.map((pokemonChoice) => (
-        <PokemonInfo name={pokemonChoice.name} image={pokemonChoice.sprites.front_default}/>
+        <PokemonInfo
+          name={pokemonChoice.name}
+          image={pokemonChoice.sprites.front_default}
+          type={pokemonChoice.types[0].type.name}
+          key={pokemonChoice.name}
+        />
       ))}
     </div>
   );
--- Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonInfoComponents/PokemonInfo.jsx
+++ Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonInfoComponents/PokemonInfo.jsx
@@ -1,10 +1,12 @@
 import styles from './PokemonInfo.module.css'
 
-export default function PokemonInfo({ name, image }) {
+export default function PokemonInfo({ name, image, type, weight }) {
   return (
     <div className={styles.container}>
       <p className={styles.name}>{name}</p>
       <img className={styles.pokemonImg} src={image} alt='pokemon image' />
+      <p>Type: {type}</p>
+      <p>Weight: {weight}</p>
     </div>
   );
 }
