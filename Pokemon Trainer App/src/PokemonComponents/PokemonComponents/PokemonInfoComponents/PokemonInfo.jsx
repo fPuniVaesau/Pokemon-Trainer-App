d29e5e67dc83c9b468e4129856aecfb52c1df78e
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-27 17:09:49 -0700

    Working on the card component for the application.

--- Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonInfoComponents/PokemonInfo.jsx
+++ Pokemon Trainer App/src/PokemonComponents/PokemonComponents/PokemonInfoComponents/PokemonInfo.jsx
@@ -1,12 +1,14 @@
-import styles from './PokemonInfo.module.css'
+import styles from './PokemonInfo.module.css';
 
 export default function PokemonInfo({ name, image, type, weight }) {
   return (
     <div className={styles.container}>
       <p className={styles.name}>{name}</p>
       <img className={styles.pokemonImg} src={image} alt='pokemon image' />
-      <p>Type: {type}</p>
-      <p>Weight: {weight}</p>
+      <div className={styles.statsContainer}>
+        <p>Type: {type}</p>
+        <p>Weight: {weight}</p>
+      </div>
     </div>
   );
 }
